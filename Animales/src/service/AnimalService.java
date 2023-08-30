c4258a8478cec16d72b7d3ccb347d160e595acc7
Jose Armando Avendaño Saucedo <126033004+ja12as@users.noreply.github.com>
2023-08-30 14:18:16 -0500

    asignado codigo

--- Animales/src/service/AnimalService.java
+++ Animales/src/service/AnimalService.java
@@ -9,5 +9,54 @@ package service;
  * @author Lenovo
  */
 public class AnimalService {
-    
+    public class VistaAnimales {
+    public void imprimirGato(String nombre, int edad, String raza) {
+        System.out.println("Gato:");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Raza: " + raza);
+        System.out.println();
+    }
+
+    public void imprimirPerro(String nombre, int edad, String raza) {
+        System.out.println("Perro:");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Raza: " + raza);
+        System.out.println();
+    }
+
+    public void imprimirCaballo(String nombre, int edad, String raza) {
+        System.out.println("Caballo:");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Raza: " + raza);
+        System.out.println();
+    }
+
+    public void imprimirConejo(String nombre, int edad, String raza) {
+        System.out.println("Conejo:");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Raza: " + raza);
+        System.out.println();
+    }
+
+    public void imprimirPajaro(String nombre, int edad, String especie) {
+        System.out.println("Pájaro:");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Especie: " + especie);
+        System.out.println();
+    }
+
+    public void imprimirPato(String nombre, int edad, String especie) {
+        System.out.println("Pato:");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Especie: " + especie);
+        System.out.println();
+    }
+}
+
 }
