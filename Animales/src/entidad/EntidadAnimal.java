ca8620bc5d9b380b84b60bd1eb17de01e7ed5611
Monica Limas Sarasty <molimasarasty@gmail.com>
2023-08-30 19:03:16 -0500

    hola

--- Animales/src/entidad/EntidadAnimal.java
+++ Animales/src/entidad/EntidadAnimal.java
@@ -14,8 +14,87 @@ public class EntidadAnimal {
    
     
   // codigo de laura "conejo" y "caballo"
+  public class Caballo {
+    private String nombre;
+    private int edad;
+    private String raza;
+
+    public Caballo(String nombre, int edad, String raza) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.raza = raza;
+    }
+
+        public Caballo() {
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public int getEdad() {
+            return edad;
+        }
+
+        public void setEdad(int edad) {
+            this.edad = edad;
+        }
+
+        public String getRaza() {
+            return raza;
+        }
+
+        public void setRaza(String raza) {
+            this.raza = raza;
+        }
+
     
-    
+}
+
+public class Conejo {
+    private String nombre;
+    private int edad;
+    private String raza;
+
+    public Conejo(String nombre, int edad, String raza) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.raza = raza;
+    }
+
+        public Conejo() {
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public int getEdad() {
+            return edad;
+        }
+
+        public void setEdad(int edad) {
+            this.edad = edad;
+        }
+
+        public String getRaza() {
+            return raza;
+        }
+
+        public void setRaza(String raza) {
+            this.raza = raza;
+        }
+ 
+}
+   
   // codigo de juan "pajaro" y "pato"
 
 }
--- Animales/src/service/AnimalService.java
+++ Animales/src/service/AnimalService.java
@@ -14,7 +14,21 @@ public class AnimalService {
    
     
  // codigo de laura "conejo" y "caballo"
-   
+ public void imprimirCaballo(String nombre, int edad, String raza) {
+        System.out.println("Caballo:");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Raza: " + raza);
+        System.out.println();
+    }
+
+    public void imprimirConejo(String nombre, int edad, String raza) {
+        System.out.println("Conejo:");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Raza: " + raza);
+        System.out.println();
+    }  
     
  // codigo de juan "pajaro" y "pato"
     
